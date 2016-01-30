7c07aa05ea460e34d19a31509553517a0fcc6647
Patrick Dwyer <patricknevindwyer@gmail.com>
2016-01-30 14:13:13 -0500

    Found RIPENCC file format with opaque_id

--- data/download.sh
+++ data/download.sh
@@ -2,6 +2,6 @@
 
 curl -# -o delegated-arin-extended-latest ftp://ftp.arin.net/pub/stats/arin/delegated-arin-extended-latest
 curl -# -o delegated-apnic-extended-latest ftp://ftp.apnic.net/pub/stats/apnic/delegated-apnic-extended-latest
-curl -# -o delegated-ripencc-extended-latest ftp://ftp.ripe.net/ripe/stats/delegated-ripencc-extended-latest
+curl -# -o delegated-ripencc-new-latest ftp://ftp.ripe.net/ripe/stats/delegated-ripencc-new-latest
 curl -# -o delegated-afrinic-extended-latest ftp://ftp.afrinic.net/pub/stats/afrinic/delegated-afrinic-extended-latest
 curl -# -o delegated-lacnic-extended-latest ftp://ftp.lacnic.net/pub/stats/lacnic/delegated-lacnic-extended-latest
